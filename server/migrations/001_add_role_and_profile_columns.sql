fa66ef5d62472e7ef6ee0bd54e22d901c6c78428
Razin Rafeek <razinrafeek2011@gmail.com>
2026-05-24 15:24:33 +0530

    ^_^

--- server/migrations/001_add_role_and_profile_columns.sql
+++ server/migrations/001_add_role_and_profile_columns.sql
@@ -33,3 +33,11 @@ CREATE INDEX IF NOT EXISTS idx_users_last_login ON users(last_login);
 
 -- Create index on created_at for new user queries
 CREATE INDEX IF NOT EXISTS idx_users_created_at ON users(created_at);
+
+-- Populate username with email for existing users (at the end of the file)
+UPDATE users 
+SET username = email 
+WHERE username IS NULL OR username = '';
+
+-- Create index for faster username lookups
+CREATE INDEX IF NOT EXISTS idx_users_username ON users(username);
