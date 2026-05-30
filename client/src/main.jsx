e956f430e0ccd0134ab1003cbf8a02870dde5cef
Razin Rafeek <razinrafeek2011@gmail.com>
2026-05-30 19:02:32 +0530

    Configure Monaco Editor loader globally to use cdnjs

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { loader } from '@monaco-editor/react'
+
+// Configure Monaco Editor loader to load from cdnjs instead of jsDelivr
+loader.config({
+  paths: {
+    vs: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs'
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
